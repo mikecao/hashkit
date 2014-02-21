7bd707384d5ecdb8ae40689a887fbaaf4b0a0831
Mike Cao <mike@mikecao.com>
2014-02-20 20:35:15 -0800

    Added test for seeding.

--- test/test.js
+++ test/test.js
@@ -30,9 +30,16 @@ describe("Hashkit", function(){
     describe("character shuffling", function(){
         var h = new hashkit({"shuffle": true});
         it("should shuffle characters", function(){
-            var h = new hashkit({shuffle: true});
             assert.equal(h.options.shuffle, true);
             assert.equal(h.options.chars, "WLgvI6CN7tqi8xfdFJZYDVeuP5K0kpAhmTEG2SjH39cXoyMzQbsraUBwl1OR4n");
+            assert.equal(h.encode(1), "L");
+            assert.equal(h.encode(10), "q");
+            assert.equal(h.encode(100), "Lj");
+            assert.equal(h.encode(1000), "F7");
+            assert.equal(h.decode("L"), 1);
+            assert.equal(h.decode("q"), 10);
+            assert.equal(h.decode("Lj"), 100);
+            assert.equal(h.decode("F7"), 1000);
         });
     });
 
@@ -40,7 +47,24 @@ describe("Hashkit", function(){
         var h = new hashkit({"mask": true});
         it("should mask the real numbers", function(){
             assert.equal(h.options.mask, true);
-            assert.notEqual(h.encode(1), "b");
+            assert.equal(h.encode(1), "cop");
+            assert.equal(h.encode(10), "xjO");
+            assert.equal(h.encode(100), "bcrE");
+            assert.equal(h.encode(1000), "KKTs");
+            assert.equal(h.decode("cop"), 1);
+            assert.equal(h.decode("xjO"), 10);
+            assert.equal(h.decode("bcrE"), 100);
+            assert.equal(h.decode("KKTs"), 1000);
+        });
+    });
+
+    describe("random seeding", function(){
+        var h = new hashkit({"shuffle": true, "mask": true, "seed": 99999});
+        it("should randomize the output", function(){
+            assert.equal(h.options.chars, "yWjaFqLb8uIliZz7vr2REC30MPUhSxtpY1J6TfswGcmXdV9KDnegoHQkAN54OB");
+            assert.equal(h.encode(1), "WT7");
+            assert.equal(h.encode(2), "AO");
+            assert.equal(h.encode(3), "UB");
         });
     });
 });
